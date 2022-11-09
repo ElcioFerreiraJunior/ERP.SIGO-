76f2d9351288b40424996680fc843ca7a6a2600a
ElcioFerreiraJunior <115508317+ElcioFerreiraJunior@users.noreply.github.com>
2022-11-09 20:18:07 -0300

    first commit

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useContext, useState} from "react"; 
+
+import { AuthContext } from "../../contexts/auth.jsx/auth";
+
+import { getUsers } from "../../services/api";
+
+const HomePage = () => {
+    const {logout } = useContext(AuthContext);
+    const [users, setUser] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => { 
+        (async () => {
+            const response = await getUsers();
+            setUser(response.data);
+            setLoading(false); 
+        })();
+    },[] );
+
+    const handleLogout = () => {
+        logout();
+
+    };
+
+    if(loading){
+        return <div className="loading">Carregando dados...</div>;
+    }
+
+    return(
+        <>
+         <h1>HomePage</h1>
+         <button onClick={handleLogout}>Logout</button> 
+         <ul>
+
+            { users.map((user) => (
+
+                    <li key = {user._id}>
+                        {user._id} - {user.email}
+                    </li>
+                ))}
+         </ul>
+         </>
+    );
+};
+
+export default HomePage; 
